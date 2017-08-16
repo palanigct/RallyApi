1d8bfad9febc74f0f485655dcdd1967ec19b85bf
ab71791 <ab71791@XVMNOCLNRM224.ctl.intranet>
2017-08-16 16:55:33 +0530

    patterns

---
+++ RallyApi 5.12.17/src/test/java/Patterns/Patterns_App.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class Patterns_App 
+{
+	public static void main()
+    {
+		Patterns_App pattern=new Patterns_App();
+	
+    }
+	
+	public void pattern_1(int width,int height)
+	{
+		
+	}
+	
+}
