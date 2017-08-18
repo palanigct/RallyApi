368441b0f1c3c490cc88d93a6aa7686a0e5d1531
ab71791 <ab71791@XVMNOCLNRM224.ctl.intranet>
2017-08-18 20:11:10 +0530

    design upto 85

--- RallyApi 5.12.17/src/test/java/Patterns/Designs.java
+++ RallyApi 5.12.17/src/test/java/Patterns/Designs.java
@@ -11,7 +11,127 @@ public class Designs
 	    int height=5;
 
 	    //System.out.println(common.abs(-10)+" "+common.abs(20));
-	   // pattern.pattern_79(width,height);   System.out.println(" ");	    
-	    //pattern.pattern_80(width, height);	   
+	    pattern.pattern_84(width,height);   System.out.println(" ");	    
+	    pattern.pattern_85(width, height);	   
     }
+	
+	public void pattern_81(int width,int height)
+	{	
+		/*
+		
+	      *     
+	     * *    
+	    *   *   
+	   *     *  
+	  *       * 
+		
+		*/
+		for(int i=0;i<height;i++)
+		{
+			for(int j=height;j>=-height;j--)
+			{
+				if(common.abs(j)==i)
+				   System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+
+	public void pattern_82(int width,int height)
+	{	
+		/*
+	      1     
+	     2 2    
+	    3   3   
+	   4     4  
+	  5       5 
+	  
+	  */
+		for(int i=0;i<height;i++)
+		{
+			for(int j=height;j>=-height;j--)
+			{
+				if(common.abs(j)==i)
+				   System.out.print(i+1);
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+
+	public void pattern_83(int width,int height)
+	{	
+		/*
+	      5     
+	     4 4    
+	    3   3   
+	   2     2  
+	  1       1 
+		
+		*/
+		
+		for(int i=0;i<height;i++)
+		{
+			for(int j=height;j>=-height;j--)
+			{
+				if(common.abs(j)==i)
+				   System.out.print(height-i);
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public void pattern_84(int width,int height)
+	{	
+		/*
+	      A     
+	     B B    
+	    C   C   
+	   D     D  
+	  E       E 
+	  
+	  */
+		for(int i=0;i<height;i++)
+		{
+			for(int j=height;j>=-height;j--)
+			{
+				if(common.abs(j)==i)
+				   System.out.print((char)(i+65));
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public void pattern_85(int width,int height)
+	{	
+		/*
+	      E     
+	     D D    
+	    C   C   
+	   B     B  
+	  A       A
+	  
+	  */
+		for(int i=0;i<height;i++)
+		{
+			for(int j=height;j>=-height;j--)
+			{
+				if(common.abs(j)==i)
+				   System.out.print((char)(height-i+64));
+				else
+					System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+
+
+
 }
