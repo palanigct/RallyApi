8210b5a9cb0d470ba03d2e456b233a7c71cbc655
ab71791 <ab71791@XVMNOCLNRM224.ctl.intranet>
2017-08-18 16:29:54 +0530

    string replace duplicate

---
+++ RallyApi 5.12.17/src/test/java/Strings/Replace_duplicate.java
@@ -0,0 +1,84 @@
+package Strings;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Replace_duplicate 
+{
+
+	public static  int[] exist_char =new int[500];
+	public static Replace_duplicate replace_duplicate = new Replace_duplicate();
+	
+	public static void main(String arg[])
+	{
+		
+		String str="cfgzz8999";
+		replace_duplicate.replace(str);
+	
+		
+		Arrays.fill(exist_char, 0);
+		
+		System.out.println("Enter the input string : ");
+		Scanner get=new Scanner(System.in);
+		str=get.next();
+		System.out.println(" out : "+str);
+		replace_duplicate.replace(str);
+		
+	}
+	
+	public void replace(String str)
+	{		
+		String final_string="";			
+		char ans=str.charAt(0);		
+		int count=0;
+		
+		final_string +=ans;
+		exist_char[(int)ans]++;	
+		
+		
+		for(int i=1;i<str.length();i++)
+		{
+			ans=str.charAt(i);
+			count=0;
+			boolean flag=true;
+			
+			while(flag==true&&count<27)
+			{				
+				count++;
+				if(exist_char[(int)ans]==0) 
+				{ 
+					 flag=false;
+				}
+				else 
+				{
+					if(ans=='z')
+						ans='a';
+					else if(ans=='Z')
+						ans='A';
+					else if(ans=='9')
+						ans='0';
+					else
+					   ans++; 				
+				}							
+			}//end-while
+			
+			final_string +=ans;
+			exist_char[(int)ans]++;
+			
+		}// end-for
+		
+		System.out.println(str+ " "+final_string);
+	}
+	
+	public char resultChar(char ch)
+	{
+		char ans=' ';		
+		if(exist_char[(int)ch]!=0) ans=ch++;
+		else ans=ch;		
+		return ans;
+	}
+	
+	
+}
+
+
