f56047f37db98d4aa87c5af2534643a718ef3400
ab71791 <ab71791@XVMNOCLNRM224.ctl.intranet>
2017-08-22 20:31:40 +0530

    missing renames

---
+++ RallyApi 5.12.17/src/test/java/Patterns/PatternsApp_3.java
@@ -0,0 +1,16 @@
+package Patterns;
+
+public class PatternsApp_3 
+{
+	public static void main(String arg[])
+    {
+		PatternsApp_3 pattern=new PatternsApp_3();
+	    int width=5;
+	    int height=5;
+		
+	   // pattern.pattern_32(width,height);   System.out.println(" ");
+	    //pattern.pattern_33(width, height);
+		
+		
+    }
+}
